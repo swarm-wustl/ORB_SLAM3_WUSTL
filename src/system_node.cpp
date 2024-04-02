a56e1736aa5eb4d0fda68719655730fca8de3f53
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2024-04-01 19:58:28 -0500

    Started getting IMU data to work in ORB SLAM

--- src/system_node.cpp
+++ src/system_node.cpp
@@ -6,6 +6,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/image_encodings.hpp"
+#include "sensor_msgs/msg/imu.hpp"
 
 #include "vslam/msg/map.hpp"
 #include "vslam/msg/key_frame.hpp"
@@ -19,6 +20,7 @@
 
 #include <Eigen/Core>
 #include "System.h" // ORB_SLAM3
+#include "ImuTypes.h" // ORB_SLAM3
 
 using namespace std;
 
@@ -53,7 +55,12 @@ class VSLAMSystemNode : public rclcpp::Node {
 
     image_transport::Subscriber mono_image_sub_;
     rclcpp::Subscription<vslam::msg::StereoImage>::SharedPtr stereo_image_sub_;
+    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub_;
     rclcpp::Publisher<vslam::msg::Map>::SharedPtr map_publisher_;
+
+    // sensor_msgs::msg::Imu last_imu;
+    vector<ORB_SLAM3::IMU::Point> imu_buffer;
+    bool usingImu;
 
     ORB_SLAM3::System* SLAM;
 
@@ -135,7 +142,18 @@ class VSLAMSystemNode : public rclcpp::Node {
       processImage(cv_ptr->image);
 
       double tframe = msg->header.stamp.sec + msg->header.stamp.nanosec / 1000000000.0;
-      Sophus::SE3f pose = SLAM->TrackMonocular(cv_ptr->image, tframe);
+      Sophus::SE3f pose;
+      if (usingImu) {
+        if (imu_buffer.size() < 5) {
+          RCLCPP_WARN(this->get_logger(), "No IMU data available for this frame");
+        } else {
+          pose = SLAM->TrackMonocular(cv_ptr->image, tframe, imu_buffer);
+        }
+        RCLCPP_INFO(this->get_logger(), "IMU buffer size: %d", imu_buffer.size());
+        imu_buffer.clear();
+      } else {
+        pose = SLAM->TrackMonocular(cv_ptr->image, tframe);
+      }
 
       publishMap(pose);
 
@@ -178,6 +196,14 @@ class VSLAMSystemNode : public rclcpp::Node {
       cv::waitKey(3);
     }
 
+    void imuCallback(const sensor_msgs::msg::Imu::SharedPtr msg) {
+      cv::Point3f acc(msg->linear_acceleration.x, msg->linear_acceleration.y, msg->linear_acceleration.z);
+      cv::Point3f gyro(msg->angular_velocity.x, msg->angular_velocity.y, msg->angular_velocity.z);
+      double tframe = msg->header.stamp.sec + msg->header.stamp.nanosec / 1000000000.0;
+      ORB_SLAM3::IMU::Point imu_point(acc, gyro, tframe);
+      imu_buffer.push_back(imu_point);
+    }
+
   public:
     VSLAMSystemNode() : Node("vslam_system_node")
   {
@@ -199,7 +225,13 @@ class VSLAMSystemNode : public rclcpp::Node {
 
     auto stereo_video_topic_param_desc = rcl_interfaces::msg::ParameterDescriptor{};
     stereo_video_topic_param_desc.description = "Name of the topic to subscribe to for the stereo video feed";
-    this->declare_parameter<string>("stereo_video_topic", "vslam/combined", stereo_video_topic_param_desc);
+    // this->declare_parameter<string>("stereo_video_topic", "vslam/combined", stereo_video_topic_param_desc);
+    this->declare_parameter<string>("stereo_video_topic", "null", stereo_video_topic_param_desc);
+
+    auto imu_topic_param_desc = rcl_interfaces::msg::ParameterDescriptor{};
+    imu_topic_param_desc.description = "Name of the topic to subscribe to for the stereo video feed";
+    this->declare_parameter<string>("imu_topic", "imu_plugin/out", imu_topic_param_desc);
+    usingImu = this->get_parameter("imu_topic").as_string() != "null";
 
     cv::namedWindow(OPENCV_WINDOW);
 
@@ -215,6 +247,11 @@ class VSLAMSystemNode : public rclcpp::Node {
         this->get_parameter("stereo_video_topic").as_string(),
         10,
         bind(&VSLAMSystemNode::stereoCallback, this, placeholders::_1)
+    );
+    imu_sub_ = this->create_subscription<sensor_msgs::msg::Imu>(
+        this->get_parameter("imu_topic").as_string(),
+        10,
+        bind(&VSLAMSystemNode::imuCallback, this, placeholders::_1)
     );
 
     map_publisher_ = this->create_publisher<vslam::msg::Map>("vslam/map", 10);
@@ -232,10 +269,10 @@ class VSLAMSystemNode : public rclcpp::Node {
     }
 
     ORB_SLAM3::System::eSensor sensor;
-    if (!this->get_parameter("stereo_video_topic").as_string().empty()) {
-      sensor = ORB_SLAM3::System::STEREO;
+    if (this->get_parameter("stereo_video_topic").as_string() != "null") {
+      sensor = usingImu ? ORB_SLAM3::System::IMU_STEREO : ORB_SLAM3::System::STEREO;
     } else {
-      sensor = ORB_SLAM3::System::MONOCULAR;
+      sensor = usingImu ? ORB_SLAM3::System::IMU_MONOCULAR : ORB_SLAM3::System::MONOCULAR;
     }
 
     SLAM = new ORB_SLAM3::System(
